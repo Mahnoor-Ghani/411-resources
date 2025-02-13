dce9ee57e36e11ef8af5ea235635d0779473a01b
Mahnoor-Ghani <maghani@bu.edu>
2025-02-13 10:47:27 -0500

    Added app.py and dockerfile with changes

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify, make_response
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get('name', 'World')  # Get the 'name' parameter, default to 'World'
+    name = request.args.get('name', 'World')
     response = make_response(
         {
             'response': f'Hello, {name}!',
@@ -13,11 +14,16 @@ def hello():
     )
     return response
 
+@app.route('/repeat', methods=['GET'])
+def repeat():
+    input_value = request.args.get('input', '')
+    return jsonify({'body': input_value, 'status': 200})
+
 @app.route('/health', methods=['GET'])
 def health_check():
-    return jsonify(status="healthy"), 200  # Standard health check response
+    return jsonify({'body': 'OK', 'status': 200})
 
 if __name__ == '__main__':
-   
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = os.getenv('PORT', 5000)  
+    app.run(host='0.0.0.0', port=int(port), debug=True)
 
